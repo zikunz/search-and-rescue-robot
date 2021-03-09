09b1d18987cacd72910d19611695203798dc9ac7
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-03-09 13:39:36 +0800

    Add Increment: Forward, Backward, Left, Right
    The team modified robotlib.ino to make Alex make forward and backward and, turn left and right.

--- Alex/robotlib.ino
+++ Alex/robotlib.ino
@@ -9,78 +9,77 @@ typedef enum dir
 } dir;
 
 // Motor control pins
-#define M1F   5
-#define M1R   6
-#define M2F   9
-#define M2R   10
+#define M1F   6
+#define M1R   5
+#define M2F   10
+#define M2R   9
 
 
 // Ensure that percent is between 0 and 100
 double checkPercent(double percent)
 {
-  if(percent < 0.0)
+  if (percent < 0.0)
     return 0.0;
+  else if (percent > 100.0)
+    return 100.0;
   else
-    if(percent > 100.0)
-      return 100.0;
-    else
-      return percent;
+    return percent;
 }
 
 // Get PWM value based on percentage
 int getPWMValue(double percent)
 {
-  return (int)(percent/100.0 * 255.0); 
+  return (int)(percent / 100.0 * 255.0);
 }
 
 void move(double percent, int direction)
 {
-  int val=getPWMValue(checkPercent(percent));
+  int val = getPWMValue(checkPercent(percent));
 
   // Forward and reverse values for motors 1 and 2
   int M1FVal, M1RVal, M2FVal, M2RVal;
 
-  switch(direction)
+  switch (direction)
   {
     case FORWARD:
-      M1FVal=val;
-      M2FVal=val;
+      M1FVal = val;
+      M2FVal = val;
 
-      M1RVal=0;
-      M2RVal=0;
-      
-    break;
+      M1RVal = 0;
+      M2RVal = 0;
+
+      break;
 
     case BACKWARD:
-      M1FVal=0;
-      M2FVal=0;
+      M1FVal = 0;
+      M2FVal = 0;
 
-      M1RVal=val;
-      M2RVal=val;
-      
-    break;
+      M1RVal = val;
+      M2RVal = val;
+
+      break;
 
     case LEFT:
-      M1FVal=0;
-      M2FVal=val;
-      M1RVal=val;
-      M2RVal=0;
-    break;
+      M1FVal = 0;
+      M2FVal = val;
+      M1RVal = val;
+      M2RVal = 0;
+      break;
 
     case RIGHT:
-     M1FVal=val;
-     M2FVal=0;
-     M1RVal=0;
-     M2RVal=val;
-    break;
+      M1FVal = val;
+      M2FVal = 0;
+      M1RVal = 0;
+      M2RVal = val;
+      break;
 
     case STOP:
     default:
 
-    M1FVal=0;
-    M2FVal=0;
-    M1RVal=0;
-    M2RVal=0;
+      M1FVal = 0;
+      M2FVal = 0;
+      M1RVal = 0;
+      M2RVal = 0;
   }
 
   analogWrite(M1F, M1FVal);
@@ -113,4 +112,3 @@ void stop()
 {
   move(0.0, STOP);
 }
-
