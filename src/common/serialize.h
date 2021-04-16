da884b5996cf9eb503ec95e6cedb8cbe497213c6
Hu Jialun <hujialun@outlook.sg>
2021-04-17 05:26:30 +0800

    Format common

--- src/common/serialize.h
+++ src/common/serialize.h
@@ -3,19 +3,18 @@
 
 #include <stdlib.h>
 
-#define PACKET_SIZE		140
-#define MAX_DATA_SIZE				128
+#define PACKET_SIZE 140
+#define MAX_DATA_SIZE 128
 
-typedef enum
-{
-	PACKET_OK = 0,
-	PACKET_BAD = 1,
-	PACKET_CHECKSUM_BAD = 2,
-	PACKET_INCOMPLETE = 3,
-	PACKET_COMPLETE = 4
+typedef enum {
+    PACKET_OK = 0,
+    PACKET_BAD = 1,
+    PACKET_CHECKSUM_BAD = 2,
+    PACKET_INCOMPLETE = 3,
+    PACKET_COMPLETE = 4
 } TResult;
 
-int serialize(char *buffer, void *dataStructure, size_t size);
-TResult deserialize(const char *buffer, int len, void *output);
+int serialize(char* buffer, void* dataStructure, size_t size);
+TResult deserialize(const char* buffer, int len, void* output);
 
 #endif
